755ed6caaab4f55d8d4a6ef077204f6baa857556
Divyasw <divyasonu23@gmail.com>
2021-12-17 13:02:12 +0530

    login

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/augintern')
+mongoose.connect('mongodb+srv://divya_123:divya_123@cluster0.pbtgo.mongodb.net/loginapp?retryWrites=true&w=majority')
